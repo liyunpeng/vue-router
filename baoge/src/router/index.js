350392e68833fe141d08e1984da2d6e3095240fb
lijinyang <kylin27@126.com>
2020-02-10 19:40:06 +0800

    添加一个名为home的vue组件

--- baoge/src/router/index.js
+++ baoge/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import home from '@/components/home'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/home', //   访问路径： http://localhost:8080/#/home
+      component: home
     }
   ]
 })
