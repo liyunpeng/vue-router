8207fe31ebc229be7ee9a5c6b470b372fb6b822a
lijinyang <kylin27@126.com>
2020-02-12 12:19:57 +0800

    添加element ui标签el

--- baoge/src/components/home.vue
+++ baoge/src/components/home.vue
@@ -2,13 +2,17 @@
     <div>
         <h1>home</h1>
         <p>{{msg}}</p>
+        <el-radio v-model="radio" label="1">备选项</el-radio>
+        <el-radio v-model="radio" label="2">备选项</el-radio>
+
     </div>
 </template>
 <script>
 export default {
   data () {
     return {
-      msg: '我是home 组件'
+      msg: '我是home 组件',
+      radio: '1'
     }
   }
 }
