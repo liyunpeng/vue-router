0b692cd7120d270b1d01fab17d859ee5ea38c7ec
lijinyang <kylin27@126.com>
2020-02-15 20:39:15 +0800

    options用于获取data外的变量

--- baoge/src/components/slot1.vue
+++ baoge/src/components/slot1.vue
@@ -13,6 +13,16 @@ export default {
       msg: '我是home 组件',
       radio: '1'
     }
+  },
+  name: 'data外的变量',
+  age: 12,
+  func1 () {
+    console.log('func1')
+  },
+  created () {
+    console.log(this.$options.name)
+    console.log(this.$options.age)
+    this.$options.func1()
   }
 }
 </script>
