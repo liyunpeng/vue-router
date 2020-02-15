d64d39fd7123e85d1343450449a47314088011ab
lijinyang <kylin27@126.com>
2020-02-15 11:52:06 +0800

    添加promise用法

--- baoge/src/store/user.js
+++ baoge/src/store/user.js
@@ -8,6 +8,14 @@ const mutations = {
   },
   setUserAge (state, age) {
     state.age = age
+  },
+  promiseFunc () {
+    return new Promise(function (resolve, reject) {
+      setTimeout(function () {
+        console.log('执行完成')
+        resolve('随便什么数据')
+      }, 500)
+    })
   }
 }
 const actions = {}
