6f42d09a10c3df450af330ecbfc6977ddf7eed21
Your Name <kylin27@126.com>
2020-03-17 22:50:00 +0800

    解决http header authorization因缺少beaer, 被服务器认为身份错误的问题

--- baoge/src/components/axios.js
+++ baoge/src/components/axios.js
@@ -1,22 +1,22 @@
 import Vue from 'vue';
 import axios from 'axios';
 import store from '../store';
- 
+
 Vue.prototype.$axios = axios
 // axios.defaults.baseURL = 'http://localhost:5004/api';
-axios.defaults.headers.common['Authorization'] =  store.state.user.jwt_token;
+axios.defaults.headers.common['Authorization'] =  'Bearer ' + store.state.user.jwt_token;
 // axios.defaults.headers.common['Authorization'] = store.state.user.jwt_token;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.put['Content-Type'] = 'application/json';
- 
+
 // 添加请求拦截器
 axios.interceptors.request.use(function(config) {
-    config.headers.Authorization = store.state.user.jwt_token;
+    config.headers.Authorization = 'Bearer ' + store.state.user.jwt_token;
     // config.headers.Authorization =  store.state.user.jwt_token;
     console.info(config);
     return config;
 }, function(error) {
     return Promise.reject(error);
 });
- 
+
 export default axios
